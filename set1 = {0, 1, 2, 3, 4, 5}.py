43367f0c9a181bcb347f4454a9188c30671cd1ce
madhavamsharma990-commits <madhavamsharma990@gmail.com>
2026-01-19 11:46:16 +0530

    Add files via upload

---
+++ set1 = {0, 1, 2, 3, 4, 5}.py
@@ -0,0 +1,6 @@
+set1 = {0, 1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8, 9}
+symmetric_diff = set1 ^ set2
+print(f"Set 1: {set1}")
+print(f"Set 2: {set2}")
+print(f"Symmetric Difference: {symmetric_diff}")
