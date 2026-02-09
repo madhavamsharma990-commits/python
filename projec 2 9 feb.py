68baf4bb8d25d2289ee2b50a5ff33692ae2cd4c8
madhavamsharma990-commits <madhavamsharma990@gmail.com>
2026-02-09 18:41:57 +0530

    Add files via upload

---
+++ projec 2 9 feb.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+class animal(ABC):
+    def move(self):
+        pass
+class human(animal):
+    def move(self):
+        print("I can walk and run")
+class snake(animal):
+    def move(self):
+        print("I can crawl")   
+class dog(animal):
+    def move(self):
+        print("I can bark and run")
+class lion(animal):
+    def move(self):
+        print("I can roar and run")
+R = human()
+R.move()
+k = snake()
+k.move()
+R = dog()
+R.move()
+k = lion()
+k.move()
