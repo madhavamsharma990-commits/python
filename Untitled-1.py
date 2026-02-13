5a5e9c25062d58dd27ae4af8dbc3a0c5f1f6c592
madhavamsharma990-commits <madhavamsharma990@gmail.com>
2026-02-13 20:28:31 +0530

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,9 +1,24 @@
-class vehicle:
-    def __init__(self, name, max_speed, mileage):
-        self.name = name
-        self.max_speed = max_speed
-        self.mileage = mileage
-class bus(vehicle):
-    pass
-school_bus = bus("School Volvo", 180, 12)
-print("Vehicle Name:", school_bus.name, "Speed:", school_bus.max_speed, "Mileage:", school_bus.mileage)
+class BMW:
+    def fuel_type(self):
+        print("BMW uses Diesel/Petrol")
+    def max_speed(self):
+        print("Max speed is 240 mph")
+class Ferrari:
+    def fuel_type(self):
+        print("Ferrari uses Premium Petrol")
+    def max_speed(self):
+        print("Max speed is 350 mph")
+def car_details(car_object):
+    print("Car Details:")
+    car_object.fuel_type()
+    car_object.max_speed()
+    print("-" * 20)
+car1 = BMW()
+car2 = Ferrari()
+car_details(car1)
+car_details(car2)
+print("Iterating over car objects:")
+for car in (car1, car2):
+    car.fuel_type()
+    car.max_speed()
+    print("-" * 20)
