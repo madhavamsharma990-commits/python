89b98a3f2545fee930fbd3a8a05fee9431a4d86a
madhavamsharma990-commits <madhavamsharma990@gmail.com>
2026-03-09 18:49:25 +0530

    Add files via upload

--- activity.py
+++ activity.py
@@ -1,18 +1,23 @@
 import pygame
-import sys 
+
+import random
+
+# Constants for easier adjustments
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 500, 400
+
+MOVEMENT_SPEED = 5
+
+FONT_SIZE = 72
+
+# Initialize Pygame
+
 pygame.init()
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("My First Pygame Screen")
-WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: 
-            running = False
-    screen.fill(BLUE)
-    pygame.display.flip() 
-pygame.quit()
-sys.exit()
+
+# Load and transform the background image
+
+background_image = pygame.transform.scale(pygame.image.load("img.jpg"),(SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Load font once at the beginning
+
+font = pygame.font.SysFont("Times New Roman", FONT_SIZE)
